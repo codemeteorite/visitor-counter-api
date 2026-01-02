df9e1c12e5d12edcbd2e4d054a74d4d17b921fa6
Mohd Yahiya <yahiya3059@gmail.com>
2026-01-03 00:22:37 +0530

    Fix port for deployment

--- server.js
+++ server.js
@@ -24,4 +24,6 @@ app.get("/visitors", async (req, res) => {
   res.json({ count });
 });
 
-app.listen(3000, () => console.log("API running on port 3000"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("API running on port", PORT));
